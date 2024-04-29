5a7b94ab24b6447f9cf7d4105d4d63ee50bec579
di1ara <dilarayalniz1@gmail.com>
2024-04-29 16:19:36 -0400

    updated the style with tailwindscss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
